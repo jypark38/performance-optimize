e142fb6ccdda80c6ccba66d3c45d1afd274e8c47
jypark38 <jury124@naver.com>
2023-12-13 14:22:04 +0900

    컴포넌트 사전 로딩

--- olympic/src/App.js
+++ olympic/src/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, Suspense, lazy, useEffect } from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
 import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
-import ImageModal from './components/ImageModal'
+// import ImageModal from './components/ImageModal'
+
+const ImageModal = lazy(()=>import('./components/ImageModal'))
 
 function App() {
     const [showModal, setShowModal] = useState(false)
+
+    const handleMouseEnter = () => {
+        const component = import('./components/ImageModal')
+    }
+
+    useEffect(()=>{
+        const component = import('./components/ImageModal')
+    },[])
 
     return (
         <div className="App">
@@ -16,7 +26,9 @@ function App() {
             <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
             <SurveyChart />
             <Footer />
-            {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            <Suspense fallback={null}>
+                {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            </Suspense>
         </div>
     )
 }
