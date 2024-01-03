be4300463a821fb6d1494896d81e4a33d4d78836
jypark38 <jury124@naver.com>
2024-01-03 18:32:32 +0900

    3장

--- 3.homepage/src/components/Card.js
+++ 3.homepage/src/components/Card.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 function Card(props) {
+
+	const imgRef = useRef(null)
+
+	useEffect(()=>{
+		const options = {}
+		const callback = (entries, observer) => {
+			entries.forEach(entry => {
+				if(entry.isIntersecting){
+					const target = entry.target
+					const previousSibling = target.previousSibling
+
+					console.log('is intersecting', target.dataset.src)
+					previousSibling.srcset = previousSibling.dataset.srcset
+					target.src = target.dataset.src
+					observer.unobserve(target)
+				}
+			})
+		}
+		const observer = new IntersectionObserver(callback, options)
+
+		observer.observe(imgRef.current)
+
+		return () => observer.disconnect()
+	},[])
+
 	return (
 		<div className="Card text-center">
-			<img src={props.image}/>
+			<picture>
+				<source data-srcset={props.webp} type='image/webp'/>
+				<img data-src={props.image} ref={imgRef}/>
+			</picture>
 			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
 				{props.children}
 			</div>
