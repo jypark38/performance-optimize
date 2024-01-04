6f5ef56f72e0ddbbba3acc4ca2cb11a9c44a56fa
jypark38 <jury124@naver.com>
2024-01-04 23:25:58 +0900

    이미지 지연 로딩

--- 4.gallery/src/components/PhotoItem.js
+++ 4.gallery/src/components/PhotoItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { showModal } from '../redux/imageModal';
+import LazyLoad from 'react-lazyload'
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
@@ -12,16 +13,36 @@ function PhotoItem({ photo: { urls, alt } }) {
 
   return (
     <ImageWrap>
-      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      <LazyLoad offset={1000}>
+        <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      </LazyLoad>
     </ImageWrap>
   );
 }
 
-const ImageWrap = styled.div``;
+// const ImageWrap = styled.div`
+//   position: relative;
+//   width: 100%;
+//   padding-top: 56.25%; /* 16:9 */
+// `;
+
+// const Image = styled.img`
+//   cursor: pointer;
+//   position: absolute;
+//   width: 100%;
+//   height: 100%;
+//   top: 0;
+//   left: 0;
+// `;
+const ImageWrap = styled.div`
+  width: 100%;
+  aspect-ratio: 16/9;
+`;
 
 const Image = styled.img`
   cursor: pointer;
   width: 100%;
+  height: 100%;
 `;
 
 export default PhotoItem;
